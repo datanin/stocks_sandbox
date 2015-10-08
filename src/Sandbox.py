7e2bca2ba27c7c0a385ed618d1ed6cb251e48dc1
Jan Dombrowicz <jd@datanin.de>
2015-10-08 12:50:12 +0200

    added word frequency

--- src/Sandbox.py
+++ src/Sandbox.py
@@ -7,7 +7,7 @@ Programm greift auf einen Teil-Extrakt der Nachrichtensammlung zu.
 Future Import: Python 2.x Division ist nicht korrekt abgebildet
 
 @author: Jan Dombrowicz
-@version: 1.6
+@version: 1.5
 """
 from __future__ import division
 
@@ -26,7 +26,7 @@ def sandy(unternehmen):
 
     df_pos = pd.read_csv('/home/jd/2ext/stocks_prediction_1.0/positiv_neu.csv', delimiter=',', names= ['ID', 'Wort'])
     df_neg = pd.read_csv('/home/jd/2ext/stocks_prediction_1.0/negativ_neu.csv', delimiter=',', names= ['ID', 'Wort'])
-    df_news = pd.read_csv('/home/jd/2ext/stocks_prediction_1.0/data/cleaned/'+unternehmen+'.csv', delimiter=';', names= ['Titel', 'Artikel', 'Datum', 'URL', 'Unternehmen'], skiprows=1, header=None)
+    df_news = pd.read_csv('/home/jd/2ext/stocks_prediction_1.0/data/cleaned/'+unternehmen+'.csv', delimiter=';', names= ['Titel', 'Artikel', 'Datum', 'URL', 'Unternehmen'], skiprows=1, header=None, nrows=10)
     df_news = df_news.reset_index(drop=True)
 
     # DQ -> str.lower() um Wörterbuch mit Artikel zu harmonisieren
@@ -38,6 +38,7 @@ def sandy(unternehmen):
     cnt_neg = 0
     multiplikator = 1
     df_tmp = pd.DataFrame()
+    wortfreq = []
     
     # Einlesen der Nachrichten und Vorbereitung der Daten
     # Im Anschluss werden die Wörter auf die Zugehörigkeit zu einem Wörterbuch geprüft und entsprechend gewertet
@@ -46,8 +47,9 @@ def sandy(unternehmen):
         artikel_processed = artikel.lower()
         for p in list(punctuation):
             artikel_processed=artikel_processed.replace(p,' ')
-        
-        for wort in artikel_processed.split(' '):
+
+        wortliste = artikel_processed.split(' ')
+        for wort in wortliste:
             if wort in df_pos:
                 cnt_pos += (multiplikator * 1)
                 multiplikator = 1
@@ -59,18 +61,31 @@ def sandy(unternehmen):
                 print("Setze Multiplikator " + wort + " auf " + str(gewichte[wort]))
             else:
                 multiplikator = 1
-    
+
+            wortfreq.append(wortliste.count(wort))
+
         test = pd.Series([(cnt_pos/len(artikel_processed))*100, (cnt_neg/len(artikel_processed))*100, (cnt_pos/len(artikel_processed))*100 - (cnt_neg/len(artikel_processed))*100 ,len(artikel_processed)])
         cnt_pos = 0
         cnt_neg = 0
         df_tmp = df_tmp.append(test, ignore_index=True)
         
+    #print "Liste\n" + str(wortliste) + "\n"
+    #print "Frequenz\n" + str(wortfreq) + "\n"
+    #print "Paare\n" + str(zip(wortliste, wortfreq))
+
     df_tmp = df_tmp.reset_index(drop=True)
     df_tmp.columns = ["pos", "neg", "quote", "len"]
     
     # Zusammenführung der Dataframes. Fehleranfällig - Besser über einen Key joinen
     df_result = pd.concat([df_news, df_tmp], axis=1)
     
-    df_result.to_csv('/home/jd/2ext/stocks_prediction_1.0/'+unternehmen+'_bewertung.csv', sep=';', encoding='utf-8', quotechar='\'')
+    df_result.to_csv('/home/jd/dev/python/UseCase_StocksPrediction/data/'+unternehmen+'_bewertung.csv', sep=';', encoding='utf-8', quotechar='\'')
+
+    wort_dict = (dict(zip(wortliste, wortfreq)))
+    df_wortfreq = pd.DataFrame(wort_dict.items(), columns=['Wort', 'Frequenz'])
+    df_wortfreq = df_wortfreq.reset_index(drop=True)
+    df_wortfreq.to_csv('/home/jd/dev/python/UseCase_StocksPrediction/data/'+unternehmen+'_frequenz.csv', sep=';', encoding='utf-8', quotechar='\'')
+
+
 
 sandy('Adidas')
